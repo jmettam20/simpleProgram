5f03b8bcc7c3bfda1d6a65e3054a500117dda6f2
jmettam20 <jmettam20@gmail.com>
2023-06-18 09:36:18 +0100

    Applied quick fix myScanner resource leak

--- src/echo.java
+++ src/echo.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class echo {
     public static void main(String args[]){
-        Scanner myScanner = new Scanner(System.in); //scans for input
-
-        System.out.println(myScanner.nextLine()); //prints input 
+        try (Scanner myScanner = new Scanner(System.in)) {
+            System.out.println(myScanner.nextLine()); //prints input 
+        }
     }
 }
