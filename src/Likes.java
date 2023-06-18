999a6a0cd4b7a703180248a18ef4f5c214e1a355
jmettam20 <jmettam20@gmail.com>
2023-06-18 08:14:57 +0100

    Added comments

--- src/Likes.java
+++ src/Likes.java
@@ -1,5 +1,9 @@
 public class Likes {
-    public static void main(String[] args) throws Exception {
-    System.out.println("Lego, Videogames, Luna");
+    public static void main(String[] args) throws Exception {     //Start here 
+    System.out.println("Lego, Videogames, Luna");               //Thing I like go here 
     }
 }
+
+/**
+ * this is a multi line comment btw :) 
+ */
