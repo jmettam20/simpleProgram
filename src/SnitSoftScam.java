e750176887a276b6dbd1ed653aeec6c694ada22e
jmettam20 <jmettam20@gmail.com>
2023-06-18 09:56:02 +0100

    Playing around with variables and values

---
+++ src/SnitSoftScam.java
@@ -0,0 +1,13 @@
+public class SnitSoftScam {
+    public static void main(String args[]){
+        double amount;  //amount = amount of money 
+
+        amount = 5.95;
+        amount = amount + 25.00;
+
+        System.out.println("We will bill");            //Communicate to user we are charging them up the a$$ for shipping
+        System.out.print(amount);
+        System.out.println(" to your credit card");
+
+    }
+}
