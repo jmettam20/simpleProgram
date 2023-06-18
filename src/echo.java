1fca5fb45134a52024d1548701872e7e1e73a677
jmettam20 <jmettam20@gmail.com>
2023-06-18 09:33:17 +0100

    Created echo, a class that echos terminal input

---
+++ src/echo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class echo {
+    public static void main(String args[]){
+        Scanner myScanner = new Scanner(System.in); //scans for input
+
+        System.out.println(myScanner.nextLine()); //prints input 
+    }
+}
