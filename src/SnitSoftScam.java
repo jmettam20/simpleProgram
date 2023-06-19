792e10d6400be9822bbb565859c85ae26d720e2e
jmettam20 <jmettam20@gmail.com>
2023-06-19 19:33:08 +0100

    Cleaner code - more variables

--- src/SnitSoftScam.java
+++ src/SnitSoftScam.java
@@ -1,11 +1,11 @@
 public class SnitSoftScam {
     public static void main(String args[]){
         double amount = 5.95;;  //amount = amount of money 
-
-        amount = amount + 25.00;
+        double shippingAndHandling = 25.00;
+        double total = amount + shippingAndHandling;
 
         System.out.print("We will bill ");            //Communicate to user we are charging them up the a$$ for shipping
-        System.out.print(amount);
+        System.out.print(total);
         System.out.println(" to your credit card");
 
     }
