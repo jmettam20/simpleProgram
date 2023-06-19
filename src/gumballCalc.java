3c21986a001f764a56359fcc875f05246544024c
jmettam20 <jmettam20@gmail.com>
2023-06-19 19:52:34 +0100

    Made basic math dynamic

--- src/gumballCalc.java
+++ src/gumballCalc.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class gumballCalc {
     public static void main(String args[]){
-        int gumballs;                           //set veriables 
+        Scanner myScanner = new Scanner(System.in); //add scanner
+        int gumballs;                              //set veriables 
         int kids;
         int gumballsPerKid; 
 
-        gumballs = 30;                          //assign to variables 
-        kids = 4;
-        gumballsPerKid = gumballs/kids;
+        System.out.print("How many gumballs? How many kids? "); //talk to user
 
-        System.out.print("Each kid gets ");    //talk to user
+        gumballs = myScanner.nextInt();     //scanning
+        kids = myScanner.nextInt();
+
+        gumballsPerKid = gumballs/kids;         //results 
+
+
+        System.out.print("Each kid gets ");     //talk to user
         System.out.print(gumballsPerKid);
         System.out.println(" gumbaalls.");
     }
