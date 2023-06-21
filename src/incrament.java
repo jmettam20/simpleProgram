f9b4e8165175ceb70febee9d8e97639334a8a06b
jmettam20 <jmettam20@gmail.com>
2023-06-21 18:23:36 +0100

    Added incrament example

---
+++ src/incrament.java
@@ -0,0 +1,12 @@
+public class incrament {
+    public static void main(String args[]){
+        int gumballs = 27;                          //set var
+
+        ++gumballs;                                 //incrament
+
+        System.out.println(gumballs);               //print
+        System.out.println(++gumballs);             //incrament again ... 
+        System.out.println(++gumballs);
+
+    }
+}
