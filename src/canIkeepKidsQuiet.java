07836f1e25f4929e29cb521ddecd2934771bfb67
jmettam20 <jmettam20@gmail.com>
2023-06-25 09:24:04 +0100

    Added bool example

---
+++ src/canIkeepKidsQuiet.java
@@ -0,0 +1,19 @@
+public class canIkeepKidsQuiet {
+    public static void main(String args[]){
+        int gumballs = 140;
+        int kids = 15;
+        int gumballsPerKid = gumballs/kids;
+        boolean eachKidGetsTen;
+
+        System.out.print("True or False? ");
+        System.out.println("Each kid gets 10 gumballs.");
+        eachKidGetsTen = gumballsPerKid >= 10;                  // eachKidGetsTen is true if gumballsPerKid is >= 10 
+        System.out.println(eachKidGetsTen);
+
+
+
+
+
+
+    }
+}
